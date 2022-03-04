835734679886aca51e188b77a2791ca0197ca61b
a8an7a <cp.291912@gmail.com>
2022-03-04 04:18:04 +0300

    Complete styled maket

--- src/view/FormsVue.vue
+++ src/view/FormsVue.vue
@@ -1,11 +1,60 @@
 <template>
     <div class="forms">
-        <p class="a-heading">Персональные данные</p>
         <form class="forms_cover">
-            <div class="forms__field">
-                <label class="a-label" for="name">Имя</label>
-                <input class="a-text-regular" type="text" name="name">
+            <div class="forms__item personal-data">
+                <p class="a-heading a-mb-20">Персональные данные</p>
+                <div class="input__field a-mb-10">
+                    <label class="a-label" for="personalName">Имя</label>
+                    <input class="a-input a-text-regular" type="text" name="personalName">
+                </div>
+                <div class="input__field">
+                    <label class="a-label" for="personalAge">Возраст</label>
+                    <input class="a-input a-text-regular" type="text" name="personalAge">
+                </div>
+            </div>
+            <div class="forms__item children-data">
+                <div class="forms__header a-mb-10">
+                    <p class="a-heading">Дети (макс. 5)</p>
+                    <button class="a-button">
+                        <img class="a-mr-5" :src="iconPlus24" alt="icon-plus">
+                        <span>Добавить ребенка</span>
+                    </button>
+                </div>
+                <div class="forms__row a-mb-10">
+                    <div class="input__field input__field_short a-mr-18">
+                        <label class="a-label" for="childName">Имя</label>
+                        <input class="a-input a-text-regular" type="text" name="childName">
+                    </div>
+                    <div class="input__field input__field_short a-mr-18">
+                        <label class="a-label" for="childAge">Возраст</label>
+                        <input class="a-input a-text-regular" type="text" name="childAge">
+                    </div>
+                    <a href="" class="a-link a-link_action">Удалить</a>
+                </div>
+                <div class="forms__row a-mb-10">
+                    <div class="input__field input__field_short a-mr-18">
+                        <label class="a-label" for="childName">Имя</label>
+                        <input class="a-input a-text-regular" type="text" name="childName">
+                    </div>
+                    <div class="input__field input__field_short a-mr-18">
+                        <label class="a-label" for="childAge">Возраст</label>
+                        <input class="a-input a-text-regular" type="text" name="childAge">
+                    </div>
+                    <a href="" class="a-link a-link_action">Удалить</a>
+                </div>
+                <button class="a-button a-button_primary a-mt-30">Сохранить</button>
             </div>
         </form>
     </div>
 </template>
+
+<script>
+    import IconPlus24 from '@/assets/icons/plus.png'
+    export default {
+        data() {
+            return {
+                iconPlus24: IconPlus24
+            }
+        }
+    }
+</script>
--- src/view/PreviewVue.vue
+++ src/view/PreviewVue.vue
@@ -1,5 +1,19 @@
 <template>
-    <div class="a-preview">
-        <h1>Превью</h1>
+    <div class="preview-item preview-personal a-mb-60">
+        <p class="a-heading a-mb-20">Персональные данные</p>
+        <p class="a-text-bold">Василий, 30 лет</p>
+    </div>
+    <div class="preview-item preview-children">
+        <p class="a-heading a-mb-20">Дети</p>
+        <div class="preview-item__row a-mb-20">
+            <p class="a-badges">
+                <span class="a-text-bold">Петр, 10 лет</span>
+            </p>
+        </div>
+        <div class="preview-item__row a-mb-20">
+            <p class="a-badges">
+                <span class="a-text-bold">Василий, 14 лет</span>
+            </p>
+        </div>
     </div>
 </template>
